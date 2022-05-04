51520ae6eff50297bd0f51e5eabb21ebaa977ebf
Tom Gregor <tgregor@ivsoftware.com>
2022-05-03 18:31:09 -0600

    Add project files.

---
+++ devexpress_bullets_ticket/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace devexpress_bullets_ticket
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richEditControl = new DevExpress.XtraRichEdit.RichEditControl();
+            this.btnBullets = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richEditControl
+            // 
+            this.richEditControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richEditControl.Location = new System.Drawing.Point(0, 0);
+            this.richEditControl.Name = "richEditControl";
+            this.richEditControl.Size = new System.Drawing.Size(800, 550);
+            this.richEditControl.TabIndex = 0;
+            // 
+            // btnBullets
+            // 
+            this.btnBullets.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnBullets.Location = new System.Drawing.Point(12, 560);
+            this.btnBullets.Name = "btnBullets";
+            this.btnBullets.Size = new System.Drawing.Size(158, 36);
+            this.btnBullets.TabIndex = 1;
+            this.btnBullets.Text = "Bullets";
+            this.btnBullets.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.btnBullets);
+            this.Controls.Add(this.richEditControl);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DevExpress RichTextEdit";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraRichEdit.RichEditControl richEditControl;
+        private System.Windows.Forms.Button btnBullets;
+    }
+}
+
---
+++ devexpress_bullets_ticket/Form1.cs
@@ -0,0 +1,80 @@
+﻿using DevExpress.XtraRichEdit.API.Native;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace devexpress_bullets_ticket
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            richEditControl.Document.AppendText(
+                string.Join(
+                    Environment.NewLine,
+                    new string[]
+                {
+                    "Item 1",
+                    "Item 2",
+                    "Item 3",
+                    "Item 4",
+                    "Item 5",
+                }
+            ));
+            btnBullets.Click += (sender, e) =>
+            {
+                richEditControl.SelectAll();
+                execSampleCode();
+            };
+        }
+
+        private void execSampleCode()
+        {
+            var document = richEditControl.Document;
+            try
+            {
+                document.BeginUpdate();
+
+                // Create a new list pattern object 
+                AbstractNumberingList list = document.AbstractNumberingLists.Add();
+
+                //Specify the list's type 
+                list.NumberingType = NumberingType.Bullet;
+                ListLevel level = list.Levels[0];
+                level.ParagraphProperties.LeftIndent = 100;
+
+                //Specify the bullets' format 
+                //Without this step, the list is considered as numbered 
+                level.DisplayFormatString = "\u00B7";
+                level.CharacterProperties.FontName = "Symbol";
+
+                //Create a new list based on the specific pattern 
+                NumberingList bulletedList = document.NumberingLists.Add(0);
+
+                // Add paragraphs to the list 
+                document.Paragraphs.AddParagraphsToList(document.Selection, bulletedList, 0);
+
+            }
+            catch (Exception ex)
+            {
+                Debug.Assert(false, ex.Message);
+            }
+            finally
+            {
+                document.EndUpdate();
+            }
+        }
+    }
+}
